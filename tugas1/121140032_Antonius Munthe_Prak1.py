29f55ac94504e435aae3a689a0e163c5f96beb3a
antoniusmunthe <antoniusmunthe01@gmail.com>
2023-03-07 14:29:45 +0700

    Create 121140032_Antonius Munthe_Prak1.py

---
+++ tugas1/121140032_Antonius Munthe_Prak1.py
@@ -0,0 +1,14 @@
+#Antonius Munthe
+#121140032
+#PBO Rb
+
+x = int(input("Masukkan Jumlah Looping : "))
+print()
+
+
+
+for baris in range(x):
+  for kolom in range(x):
+    print('#', end = ' ')
+  else:
+    print('')
